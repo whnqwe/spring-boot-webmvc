8fa9c4e390d6f2bbc8c2cae14efd8c50efb0ee35
zhanghui <whnqwe@126.com>
2018-12-12 11:25:32 +0800

    first commit

---
+++ src/main/java/cn/zhangspace/springbootwebmvc/SpringBootWebmvcApplication.java
@@ -0,0 +1,12 @@
+package cn.zhangspace.springbootwebmvc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootWebmvcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootWebmvcApplication.class, args);
+	}
+}
---
+++ src/test/java/cn/zhangspace/springbootwebmvc/SpringBootWebmvcApplicationTests.java
@@ -0,0 +1,16 @@
+package cn.zhangspace.springbootwebmvc;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringBootWebmvcApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
